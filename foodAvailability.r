25cd10cc0ff30745f4c78ac7c98d74aad6ea735f
Nick Strayer <nstrayer@uvm.edu>
2015-02-11 21:21:16 -0500

    Added a food availability model. Now I am going to clean up the code a bit. Get all the data generating models into their own scripts and spitting to the data directory.

---
+++ foodAvailability.r
@@ -0,0 +1,24 @@
+setwd("/Users/Nick/mysisModeling")
+# Code for a simple food availability model based upon the assumptions of higher pelagic food quality
+# in the summer. 
+
+#First we set up some user defined variables. 
+min       = 0.1 #minimum food availability value
+max       = 0.9 #max 
+highDay   = 226 #Number of days into the year that the max food availability is (226 is aug 15)
+
+#Now we generate the others
+scaler    = (max - min)/2 #range divided by two
+heightAdj = (max + min)/2 #average value
+days      = 1:365 #Set up a day vector to loop over
+#A variable that ranges from 0 (totally benthic) to 1 (totally pelagic) indicating food availability.
+foodAvail = NULL  
+
+#Quick loop to generate the data. 
+for (day in days){
+  foodAvail = c(foodAvail, scaler * cos( (1/365)*2*pi * (day - highDay)) + heightAdj)
+}
+
+plot(days,foodAvail, type = "l")
+
+write.csv(foodAvail, "data/foodAvailDay.csv", row.names=FALSE)
