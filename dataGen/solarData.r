b5bf956adc0820973d1beca870940ed02712fce6
Nick Strayer <nstrayer@uvm.edu>
2015-03-08 13:01:44 -0400

    Solar data code has been trimmed and a more robust solarData_md file has been generated.

--- dataGen/solarData.r
+++ dataGen/solarData.r
@@ -4,33 +4,6 @@ setwd("/Users/Nick/mysisModeling")
 solarRaw = read.csv("data/solarData2010.csv")
 
 realSolar = solarRaw$METSTAT.Dif..Wh.m.2.
-realSolarSmoothed = smooth.spline(realSolar)
-
-# plot(realSolarSmoothed, type = "l")
-# plot(realSolar, type = "l")
-
-#Mid day
-lightAtNoon = realSolar[seq(12, length(realSolar), 24)]
-# plot(lightAtNoon, type = "l", main = "Burlington Light Levels at Mid-Day for 2010", xlab = "Day of Year", ylab = "Light Levels in Wh/m^2")
-# lines(smooth.spline(lightAtNoon), col = "blue", lw = 4)
-# axis(side = 1)
-# axis(side = 1, col = "white", tcl = 0)
-# axis(side = 2)
-# axis(side = 2, col = "white", tcl = 0)
-# png("realSolar_noon_2010.png")
-
-
-# #1 am
-# nightLight = realSolar[1000:1048]
-# plot(nightLight, type = "l", 
-#      main = "Burlington Light Levels Jan 1 - Jan 2", 
-#      xlab = "Hour", 
-#      ylab = "Light Levels in Wh/m^2")
-# axis(side = 1)
-# axis(side = 1, col = "white", tcl = 0)
-# axis(side = 2)
-# axis(side = 2, col = "white", tcl = 0)
-
 
 #Begin moonlight simulation to add to the real data: 
 #Days vector for a year
@@ -45,35 +18,17 @@ for (hour in hours){
   moonLux = c(moonLux, 0.5 * cos((1/cycle)* 2*pi * cyclePoint ) + .5)
 }
 
-#Check to see if this is functional
-# plot(hours, moonLux, type = "l",
-#      main = "Light Levels at Midnight (Moon Cycle)",
-#      xlab = "hour",
-#      ylab = "Light Level in Lumens")
-# axis(side = 1)
-# axis(side = 1, col = "white", tcl = 0)
-# axis(side = 2)
-# axis(side = 2, col = "white", tcl = 0)
-
-
-#This comes from the solar irradiance paper. Very iffy. 
 Wh_to_lumen = 120.0; #From Jensen et all. 
 combinedLight = NULL;
 solarLux = NULL;
 
 for (i in 1:(365*24)){
   solarLux = realSolar[i] * Wh_to_lumen;
-  #print(solarLux > moonLux[i])
   if (solarLux > moonLux[i]){
     combinedLight = c(combinedLight, solarLux)
   } else {
     combinedLight = c(combinedLight, moonLux[i])
   }
 }
-
-# Let's just check to make sure it's all good. 
-#plot(combinedLight[1004:1005], type = "l")
-summary(combinedLight)
-#Yup! Okay, let's output it. 
 
 write.csv(combinedLight, "data/light_day_moon_hour.csv", row.names=FALSE)
