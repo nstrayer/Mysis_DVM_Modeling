10a19ec1a786e7b257cb2f7e6f6a872106a1e077
Nick Strayer <nstrayer@uvm.edu>
2015-03-26 10:36:43 -0400

    Commit for after thursday 3/26 morning lab meeting. Food distribution now has responsive variance and some plots have been made prettier. In addition the readme has been updated with future steps.

--- dataGen/foodAvailability.r
+++ dataGen/foodAvailability.r
@@ -1,12 +1,9 @@
-setwd("/Users/Nick/mysisModeling")
 
 #First we set up some user defined variables. 
-min     = 0.1 #minimum food availability value
-max     = 0.9 #max 
-highDay = 226*24 #Number of days into the year that the max food availability is (226 is aug 15)
+min     = 0.15 #minimum food availability value
+max     = 0.97 #max 
+highDay = 210*24 #Number of days into the year that the max food availability is (226 is aug 15)
 august  = 5856 #hours into the year
-
-##########################################################################################
 
 #Now we generate the others
 scaler    = (max - min)/2 #range divided by two
@@ -19,10 +16,11 @@ for (hour in hours){
   foodAvail = c(foodAvail, scaler * cos( (1/(365*24))*2*pi * (hour - highDay)) + heightAdj)
 }
 
-##########################################################################################
 
-min     = 0.01 #minimum food availability value
-max     = 0.12 #max 
+## The variability curve
+
+min     = 0.1 #minimum food availability value
+max     = 0.9 #max 
 highDay = 100*24
 scaler    = (max - min)/2 #range divided by two
 heightAdj = (max + min)/2 #average value
@@ -33,7 +31,7 @@ for (hour in hours){
   variability = c(variability, scaler * cos( (1/(365*24))*4*pi * (hour - highDay)) + heightAdj)
 }
 
-##########################################################################################
+avail_var_df = as.data.frame(cbind(foodAvail, variability))
 
 highBound = NULL
 lowBound  = NULL
@@ -43,11 +41,10 @@ for (hour in hours){
   lowBound = c(lowBound, (foodAvail[hour] - variability[hour]))
 }
 
-#distributionDf = as.data.frame(cbind(hours, highBound, lowBound))
+distributionDf = as.data.frame(cbind(hours, highBound, lowBound))
 
-##########################################################################################
 
+setwd("/Users/Nick/mysisModeling")
 #write.csv(distributionDf, "data/FoodAvail_Hour.csv", row.names=FALSE) #The bounded model  
-write.csv(variability, "data/FoodAvail_Hour.csv", row.names=FALSE) #The plain side curve. 
-
+write.csv(avail_var_df, "data/FoodAvail_Hour.csv", row.names=FALSE) #The plain side curve. 
 
